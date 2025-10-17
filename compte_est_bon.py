720a8bf25d9eb1e8c2886efce40153b4dcac7bdd
DavidB <david.bernard@fms-ea.com>
2025-10-17 11:37:14 +0200

    numbers_to_use added inside choose_two_numbers

--- compte_est_bon.py
+++ compte_est_bon.py
@@ -35,13 +35,14 @@ def pick_six_numbers(numbers):
     return six_numbers
 
 
-numbers_to_use = pick_six_numbers(numbers_list)
+
 
 def choose_two_numbers():
     """
     Retourne une liste des deux nombres choisis
     :return: list
     """
+    numbers_to_use = pick_six_numbers(numbers_list)
     chosen_numbers = []
 
     for i in range(2):
